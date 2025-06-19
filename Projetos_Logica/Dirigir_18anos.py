bcdfd75546025b11facc3f8fb2f0aeec3f0e2d21
Kauã Souza Fortuna <kauasouzafotuna@gmail.com>
2025-06-19 18:50:06 -0300

    Remove arquivos indevidos do repositório de perfil

--- Projetos_Logica/Dirigir_18anos.py
+++
@@ -1,8 +0,0 @@
-
-idade = 12
-
-if idade >= 18:
-    print( "Você pode dirigir!")
-
-else:
-    print("Você não poderá dirigir ainda!")
--- Projetos_Logica/Protect_Solar.py
+++
@@ -1,8 +0,0 @@
-
-temperatura = 3
-
-if temperatura >= 20:
-  print ("Está na hora de passar o protetor solar")
-
-else: 
-  print ("Não está em hora de passar o protetor solar")
